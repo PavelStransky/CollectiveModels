972a9ddefa11f67f7fc2563d1ba313198dac040c
Pavel <pavel@localhost>
2017-07-27 08:23:24 +0000

    Random matrix with variable variance

--- Expression/Functions/Statistics/RandomMatrixSG.cs
+++ Expression/Functions/Statistics/RandomMatrixSG.cs
@@ -15,16 +15,19 @@ namespace PavelStransky.Expression.Functions.Def {
         public override string Help { get { return Messages.HelpRandomMatrixSG; } }
 
         protected override void CreateParameters() {
-            this.SetNumParams(1);
+            this.SetNumParams(2);
             this.SetParam(0, true, true, false, Messages.PMatrixSize, Messages.PMatrixSizeDescription, null, typeof(int));
+            this.SetParam(1, false, true, true, Messages.PVariance, Messages.PVarianceDescription, 1.0, typeof(double));
         }
 
         protected override object EvaluateFn(Guider guider, ArrayList arguments) {
             int length = (int)arguments[0];
             int ndLength = length * (length - 1) / 2;
 
-            Vector diag = this.nd.GetVector(length, System.Math.Sqrt(2.0), 0.0);
-            Vector ndiag = this.nd.GetVector(ndLength, 1.0, 0.0);
+            double var = (double)arguments[1];
+
+            Vector diag = this.nd.GetVector(length, var * System.Math.Sqrt(2.0), 0.0);
+            Vector ndiag = this.nd.GetVector(ndLength, var, 0.0);
 
             Matrix result = new Matrix(length);
             int k = 0;
